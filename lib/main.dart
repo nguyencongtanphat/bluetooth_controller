425917d910df77cd2d3b35965ad9608855dafcd1
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-03-10 15:27:13 +0700

    add file

---
+++ lib/main.dart
@@ -0,0 +1,166 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final title = 'Flutter BLE Scan Demo';
+
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  List<ScanResult> scanResultList = [];
+  bool _isScanning = false;
+
+  @override
+  initState() {
+    super.initState();
+    initBle();
+  }
+
+  void initBle() {
+    // BLE 스캔 상태 얻기 위한 리스너
+    flutterBlue.isScanning.listen((isScanning) {
+      print("is scan: $isScanning");
+      _isScanning = isScanning;
+      setState(() {});
+    });
+  }
+
+  /*
+  스캔 시작/정지 함수
+  */
+  Future<void> scan() async {
+    print("start scann");
+    if (!_isScanning) {
+      // 스캔 중이 아니라면
+      // 기존에 스캔된 리스트 삭제
+      scanResultList.clear();
+      // 스캔 시작, 제한 시간 4초
+      await flutterBlue.startScan(
+        scanMode: ScanMode.lowPower,
+        timeout: const Duration(seconds: 10),
+      );
+      // 스캔 결과 리스너
+      flutterBlue.scanResults.listen((results) {
+        print("here $results");
+        // List<ScanResult> 형태의 results 값을 scanResultList에 복사
+        scanResultList = results;
+        // UI 갱신
+        setState(() {});
+      });
+    } else {
+      // 스캔 중이라면 스캔 정지
+      flutterBlue.stopScan();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        /* 장치 리스트 출력 */
+        child: ListView.separated(
+          itemCount: scanResultList.length,
+          itemBuilder: (context, index) {
+            log('${scanResultList[index].device.id.id}\n');
+            if (scanResultList[index].device.id.id == "00:22:11:01:05:7E") {
+              return listItem(scanResultList[index]);
+            } else {
+              return const SizedBox.shrink();
+            }
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const Divider();
+          },
+        ),
+      ),
+      /* 장치 검색 or 검색 중지  */
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => scan(),
+        // 스캔 중이라면 stop 아이콘을, 정지상태라면 search 아이콘으로 표시
+        child: Icon(_isScanning ? Icons.stop : Icons.search),
+      ),
+    );
+  }
+
+/*  장치의 신호값 위젯  */
+  Widget deviceSignal(ScanResult r) {
+    return Text(r.rssi.toString());
+  }
+
+  /* 장치의 MAC 주소 위젯  */
+  Widget deviceMacAddress(ScanResult r) {
+    return Text(r.device.id.id);
+  }
+
+  /* 장치의 명 위젯  */
+  Widget deviceName(ScanResult r) {
+    String name = '';
+
+    print("device $r");
+    if (r.device.name.isNotEmpty) {
+      // device.name에 값이 있다면
+      name = r.device.name;
+    } else if (r.advertisementData.localName.isNotEmpty) {
+      // advertisementData.localName에 값이 있다면
+      name = r.advertisementData.localName;
+    } else {
+      // 둘다 없다면 이름 알 수 없음...
+      name = 'No name';
+    }
+    return Text(name);
+  }
+
+  /* BLE 아이콘 위젯 */
+  Widget leading(ScanResult r) {
+    return const CircleAvatar(
+      backgroundColor: Colors.cyan,
+      child: Icon(
+        Icons.bluetooth,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  /* 장치 아이템을 탭 했을때 호출 되는 함수 */
+  void onTap(ScanResult r) {
+    // 단순히 이름만 출력
+    print(r.device.name);
+  }
+
+  /* 장치 아이템 위젯 */
+  Widget listItem(ScanResult r) {
+    return ListTile(
+      onTap: () => onTap(r),
+      leading: leading(r),
+      title: deviceName(r),
+      subtitle: deviceMacAddress(r),
+      trailing: deviceSignal(r),
+    );
+  }
+}
